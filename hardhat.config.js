37509d86be0c4fadafc92b712205634782b4df13
Pere Joan Palou Pons <PJPalou@kabel.es>
2022-06-01 09:46:30 +0200

    Initial project environment setup

---
+++ hardhat.config.js
@@ -0,0 +1,70 @@
+require("dotenv").config({ path: "./.env" });
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+
+const { INFURA_KEY, MNEMONIC, PK } =
+  process.env;
+const DEFAULT_MNEMONIC = "hello darkness my old friend";
+
+const sharedNetworkConfig = {};
+if (PK) {
+  sharedNetworkConfig.accounts = [PK];
+} else {
+  sharedNetworkConfig.accounts = {
+    mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
+  };
+}
+
+module.exports = {
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    deploy: "deploy",
+    sources: "contracts",
+    imports: "imports",
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      ...sharedNetworkConfig,
+      blockGasLimit: 100000000,
+      gas: 2000000,
+      saveDeployments: true,
+    },
+    hardhat: {
+      blockGasLimit: 10000000000000,
+      gas: 200000000000,
+      saveDeployments: true
+    },
+    mainnet: {
+      ...sharedNetworkConfig,
+      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+      saveDeployments: true,
+    },
+    rinkeby: {
+      ...sharedNetworkConfig,
+      url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
+      saveDeployments: true,
+    },
+    ganache: {
+      ...sharedNetworkConfig,
+      url: "http://127.0.0.1:7545",
+      saveDeployments: false,
+    }
+  },
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer:{
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  namedAccounts: {
+    root: 0,
+    beneficiary: 1
+  },
+};
