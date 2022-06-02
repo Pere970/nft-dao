fa4c32ade9bf743e92c62f7a00f6fe82b3b69293
Pere Joan Palou Pons <PJPalou@kabel.es>
2022-06-02 10:33:21 +0200

    Added solidity coverage (90% avg). Added tasks to package.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-deploy");
 require("hardhat-deploy-ethers");
+require("solidity-coverage");
 
 const { INFURA_KEY, MNEMONIC, PK } =
   process.env;
